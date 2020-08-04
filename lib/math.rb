efce762bc61319b2ba98e6610573341101437f17
Daniel Turner <danielturner184@gmail.com>
2020-08-04 07:32:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,11 +8,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  divi = num1 / num2
+  return divi
 end
 
 def multiplication(num1, num2)
-
+  nulit = num1 * num2
+  return mulit
 end
 
 def modulo(num1, num2)
